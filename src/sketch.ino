a6ad5c34c6792dea90aff7dac33bc10d8d1e5464
Zakaria ElQotbi <zakaria@elqotbi.com>
2017-07-09 16:22:31 +0200

    rewrite code in a logical functional blocks

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,103 +1,120 @@
 #include <TimerOne.h>
 
-const int ledPin = 13;     // Led pin
-const int ButtonPin = 2;     // Sensor pin
-const int onoffPin = 3;      // On/off pin  connect to transistor1 base with 1K resistor
-const int shutterPin = 4;    // Shutter pin  connect to transistor2 base with 1K resistor
+const int     ledPin = 13;   // Led pin
+const int   onoffPin = 8;    // On/off button pin
+const int shutterPin = 3;    // camera/video button pin
 
-bool capturing = false;
-bool startcapturing = false;
-bool stopcapturing = false;
+#define ON   HIGH
+#define OFF  LOW
+
+void CAMERA_TURNON()
+{
+    Serial.print((float)millis()/1000,6); 
+    Serial.println("=== initiate turnon sequence ===");
+    digitalWrite(ledPin, ON);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Push on/off button for 5sec (long press) ...");
+    digitalWrite(onoffPin, ON);    //Push on/off button 3sec for turn camcorder ON
+
+    delay(5000);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Release ON/OFF Button");
+    digitalWrite(onoffPin, OFF);    //Release button
+    delay(3000);
+    //YelOFF led on camera should be on
+}
+
+void CAMERA_TURNOFF()
+{
+    Serial.print((float)millis()/1000,6); 
+    Serial.println("=== initiate turnoff sequence ===");
+    digitalWrite(ledPin, ON);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Push on/off button for 5sec (long press)...");
+    digitalWrite(onoffPin, ON);    //Push on/off button 5sec for turn camcorder ON
+
+    delay(5000);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Release ON/OFF Button");
+    digitalWrite(onoffPin, OFF);    //Release button
+    delay(3000);
+    //YelOFF led on camera should be off
+}
+
+
+void CAMERA_STARTREC()
+{
+    Serial.print((float)millis()/1000,6); 
+    Serial.println("=== intiate video recording start sequence ===");
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Push video/camera button 3sec (long press) ...");
+    digitalWrite(shutterPin, ON);    //Push video/camera button for 10sec to start recording. 
+
+    delay(3000);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Release video/camera Button");
+    digitalWrite(shutterPin, OFF);    //Release button
+    //YelOFF led should flash 3 times and off. 
+}
+
+void CAMERA_STOPREC()
+{
+    Serial.print((float)millis()/1000,6); 
+    Serial.println("=== initiate video recording stop sequence ===");
+    digitalWrite(ledPin, ON);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Push video/camera button for 300 msec (sort press)...");
+    digitalWrite(shutterPin, ON);    //Push video/camera button for 300msec to stop recording. 
+
+    delay(300);
+
+    Serial.print((float)millis()/1000,6); 
+    Serial.println(" Release video/camera Button");
+    digitalWrite(shutterPin, OFF);    //Release button
+    //YelOFF led on camera should be on
+}
+
+void CAMERA_WAIT()
+{
+    Serial.print((float)millis()/1000,6); 
+    Serial.println("=== recording for 60 secs ===");
+    delay(60000); //60sec
+}
 
 void setup() {
   pinMode(onoffPin, OUTPUT);
+  digitalWrite(onoffPin, OFF);
+
   pinMode(ledPin, OUTPUT);
-  pinMode(ButtonPin, INPUT);
+  digitalWrite(ledPin, OFF);
+
   pinMode(shutterPin, OUTPUT);
+  digitalWrite(shutterPin, OFF);
+
   Serial.begin(9600);
   while (!Serial) ; // Wait for serial port to be available 
-  Timer1.initialize(180000000);
-  Timer1.attachInterrupt(togglecapturing); // attach the service routine here
 }
 
-int toverflow = 0;
-void checktimer()
-{
-    if(toverflow++<3) return;
-    toverflow=0;
-
-    Serial.print((float)millis()/1000,6); 
-}
-
-void togglecapturing()
-{
-    checktimer();
-    Serial.println(" timer handler");
-    //Serial.print("Set capture to ");
-    if(capturing) {
-    	//Serial.println("OFF");
-	//stopcapturing = true;
-
-    } else {
-    	//Serial.println("ON");
-	startcapturing = true;
-    }
-    //capturing = !capturing;
-}
-
+#define MAX 100
 void loop()
 {
-    delay(500);
-    Serial.println("=== looping ===");
-    if(startcapturing){
-    noInterrupts();
-	startcapturing = false;
-    interrupts();
-	Serial.println("=== Button pushed, initiate start sequence ===");
-	capturing = true;
-	digitalWrite(ledPin, HIGH);
-	delay(300);
+    CAMERA_TURNON();
 
-	digitalWrite(onoffPin, HIGH);    //Push on/off button 3sec for turn camcorder ON
-	Serial.println("Push on/off button 3sec...");
-
-	delay(3000);
-	digitalWrite(onoffPin, LOW);    //Release button
-	Serial.println("Release ON/OFF Button");
-	delay(3000);
-
-	digitalWrite(shutterPin, HIGH);  //Push record button 3 second to start video
-	Serial.println("Push REC button 3 sec...");
-	delay(3000);
-	digitalWrite(shutterPin, LOW);    //Release button
-	Serial.println("Release REC button");
-    }
-    if (stopcapturing) {
-    noInterrupts();
-	stopcapturing = false;
-    interrupts();
-	Serial.println("=== Button pushed, initiate stop sequence ===");
-	digitalWrite(ledPin, LOW);
-	capturing = false;
-
-	digitalWrite(shutterPin, HIGH);  //Push record button once to stop video
-	delay(300);
-	Serial.println("Push REC button once");
-	digitalWrite(shutterPin, LOW);    //Release button
-	delay(2000);                      //Wait for copy to SD Card
-	Serial.println("Should be copying to SD card...");
-	// Shutdown
-	digitalWrite(onoffPin, HIGH);    //Push on/off button 3sec for turn camcorder OFF
-	Serial.println("Push OFF button 3sec");
-	delay(3000);
-	digitalWrite(onoffPin, LOW);     //Release button
-	Serial.println("Camera should be turned off");
-	digitalWrite(ledPin, LOW);
+    for (int i=0;i<MAX;i++) {
+	CAMERA_STARTREC();
+	CAMERA_WAIT();
+	CAMERA_STOPREC();
     }
 
-    if(capturing){
-	digitalWrite(ledPin, !digitalRead(ledPin));
-    }
+    delay(1000);
+    CAMERA_TURNOFF();
 
-    delay(100);
+    while(1);
 }
